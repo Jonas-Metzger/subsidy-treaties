c085a97df54cd0d9df2a0a5aad557093a06d8e19
Jonas Metzger <40152568+Jonas-Metzger@users.noreply.github.com>
2023-05-05 19:39:59 -0700

    added averaged data

--- data/projections/download.sh
+++
@@ -1,4 +0,0 @@
-!wget https://zenodo.org/record/6016583/files/rffsps_v5.7z
-!7za x archive.7z -r -ir!rffsp_pop_in* -o./pop_income
-!7za x archive.7z -r -ir!*emissions.csv -o./emissions
-!7za x archive.7z -r -ir!README.md
